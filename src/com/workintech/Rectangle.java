6b26b878fb765770ce9b56754e31994fdbf6087c
Kadiraydn01 <kadiraydn01@gmail.com>
2023-09-25 13:35:51 +0300

    İkinci Bölüm Tamam

--- src/com/workintech/Rectangle.java
+++ src/com/workintech/Rectangle.java
@@ -1,4 +1,27 @@
 package com.workintech;
 
 public class Rectangle {
+    private double width;
+    private double length;
+
+    public Rectangle(double width , double length){
+        this.width = width;
+        if(width < 0){
+            width = 0;
+        };
+        this.length = length;
+        if(length < 0){
+            length = 0;
+        };
+    };
+    public double getWidth() {
+        return width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+    public double getArea(){
+        return width * length;
+    }
 }
